3d86ebd62d34f53323fd5aa3422c3ae3c0c61aa6
Mark Valentino <mike9377mike9377@gmail.com>
2024-06-03 21:33:49 -0400

    filled out reviews

--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import Image from "next/image";
 
 const reviews = [
-  { imgSrc: "/pexels-artempodrez-8533095.png", name: "fs", title: "rw", description: "Acme Chemicals has been a reliable supplier for our\n laboratory. The quality of their products is top-notch\n and their customer service is exceptional." },
-  { imgSrc: "/pexels-artempodrez-8533095.png", name: "fs", title: "rew", description: "" },
-  { imgSrc: "/pexels-artempodrez-8533095.png", name: "fd", title: "rw", description: "" },
-  { imgSrc: "/pexels-artempodrez-8533095.png", name: "fs", title: "rewre", description: "" },
+  { imgSrc: "/pexels-artempodrez-8533095.png", name: "Jane Dursel", title: "Production Manager", description: "Acme Chemicals has been a reliable supplier for our laboratory. The quality of their products is top-notch and their customer service is exceptional." },
+  { imgSrc: "/pexels-artempodrez-8533095.png", name: "Dr. Priya Mehta", title: "Senior Research Scientist", description: "ACME Chemicals consistently delivers high-quality products on time, making them an invaluable partner for our research projects. Their customer service team is knowledgeable and always ready to assist." },
+  { imgSrc: "/pexels-artempodrez-8533095.png", name: "Jonathan Burke", title: "Chemical Engineer", description: "I\'ve been using Acme Chemicals for years and their products have always exceeded my expectations. The fast delivery and competitive pricing make them a top choice for our business." },
+  { imgSrc: "/pexels-artempodrez-8533095.png", name: "Joseph Mwangi", title: "Procurement Manager", description: "We have been sourcing our laboratory chemicals from ACME Chemicals for over five years, and their reliability is unmatched. Their competitive pricing and product range have been key to our operational success." },
 ];
 
 interface ReviewProps {
@@ -32,10 +32,10 @@ const Review: React.FC<ReviewProps> = (props: ReviewProps) => {
         />
         <div className=" ml-4">
           <div className="font-semibold">{name}</div>
-          <div className="text-sm">{title}</div>
+          <div className="text-sm text-gray-700">{title}</div>
         </div>
       </div>
-      <p className="mt-2">
+      <p className="mt-2 text-gray-700">
        {description}
       </p>
     </div>
