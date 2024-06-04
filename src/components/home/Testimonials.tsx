390f35854ead7af9efb1b0934a3d9795203500b7
Mark Valentino <mike9377mike9377@gmail.com>
2024-06-03 21:42:32 -0400

    made reviews go in grid

--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -2,10 +2,34 @@ import React from "react";
 import Image from "next/image";
 
 const reviews = [
-  { imgSrc: "/pexels-artempodrez-8533095.png", name: "Jane Dursel", title: "Production Manager", description: "Acme Chemicals has been a reliable supplier for our laboratory. The quality of their products is top-notch and their customer service is exceptional." },
-  { imgSrc: "/pexels-artempodrez-8533095.png", name: "Dr. Priya Mehta", title: "Senior Research Scientist", description: "ACME Chemicals consistently delivers high-quality products on time, making them an invaluable partner for our research projects. Their customer service team is knowledgeable and always ready to assist." },
-  { imgSrc: "/pexels-artempodrez-8533095.png", name: "Jonathan Burke", title: "Chemical Engineer", description: "I\'ve been using Acme Chemicals for years and their products have always exceeded my expectations. The fast delivery and competitive pricing make them a top choice for our business." },
-  { imgSrc: "/pexels-artempodrez-8533095.png", name: "Joseph Mwangi", title: "Procurement Manager", description: "We have been sourcing our laboratory chemicals from ACME Chemicals for over five years, and their reliability is unmatched. Their competitive pricing and product range have been key to our operational success." },
+  {
+    imgSrc: "/pexels-artempodrez-8533095.png",
+    name: "Jane Dursel",
+    title: "Production Manager",
+    description:
+      "Acme Chemicals has been a reliable supplier for our laboratory. The quality of their products is top-notch and their customer service is exceptional.",
+  },
+  {
+    imgSrc: "/pexels-artempodrez-8533095.png",
+    name: "Dr. Priya Mehta",
+    title: "Senior Research Scientist",
+    description:
+      "ACME Chemicals consistently delivers high-quality products on time, making them an invaluable partner for our research projects. Their customer service team is knowledgeable and always ready to assist.",
+  },
+  {
+    imgSrc: "/pexels-artempodrez-8533095.png",
+    name: "Jonathan Burke",
+    title: "Chemical Engineer",
+    description:
+      "I've been using Acme Chemicals for years and their products have always exceeded my expectations. The fast delivery and competitive pricing make them a top choice for our business.",
+  },
+  {
+    imgSrc: "/pexels-artempodrez-8533095.png",
+    name: "Joseph Mwangi",
+    title: "Procurement Manager",
+    description:
+      "We\'ve been sourcing our laboratory chemicals from ACME Chemicals for over five years, and their reliability is unmatched. Their competitive pricing and product range have been key to our success.",
+  },
 ];
 
 interface ReviewProps {
@@ -24,20 +48,13 @@ const Review: React.FC<ReviewProps> = (props: ReviewProps) => {
       key={key}
     >
       <div className=" flex flex-row">
-        <Image
-          src={imgSrc}
-          alt="avatar"
-          width={56}
-          height={56}
-        />
+        <Image src={imgSrc} alt="avatar" width={56} height={56} />
         <div className=" ml-4">
           <div className="font-semibold">{name}</div>
           <div className="text-sm text-gray-700">{title}</div>
         </div>
       </div>
-      <p className="mt-2 text-gray-700">
-       {description}
-      </p>
+      <p className="mt-2 text-gray-700">{description}</p>
     </div>
   );
 };
@@ -52,15 +69,17 @@ export const Testimonials = () => {
         Hear from our satisfied customers about their experience with Acme
         Chemicals.
       </p>
-      {reviews.map((rev, index: number) => (
-        <Review
-          key={index}
-          imgSrc={rev.imgSrc}
-          name={rev.name}
-          title={rev.title}
-          description={rev.description}
-        />
-      ))}
+      <div className="md:grid md:grid-cols-2 md:gap-2">
+        {reviews.map((rev, index: number) => (
+          <Review
+            key={index}
+            imgSrc={rev.imgSrc}
+            name={rev.name}
+            title={rev.title}
+            description={rev.description}
+          />
+        ))}
+      </div>
     </div>
   );
 };
