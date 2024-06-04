1346a59bc3c7d104d27d73050eaa1daabe983cf8
Mark Valentino <mike9377mike9377@gmail.com>
2024-06-03 21:21:33 -0400

    converted reviews to map

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -18,7 +18,7 @@ export const ProductCard: React.FC<ProductCardProps> = (
   return (
     <div className=" min-w-[280px] m-4 max-w-[280px] rounded-md shadow-sm p-4 border-slate-150 border-2" key={key}>
       {discount && (
-        <div className="relative -mb-4 -mt-4 h-8 leading-8 font-bold rounded-sm w-fit pl-2 pr-2 bg-green-700 text-white"
+        <div className="relative -mb-4 -mt-4 h-8 leading-8 font-bold rounded-2xl w-fit pl-2 pr-2 bg-green-700 text-white"
         >
           {discount}% off
         </div>
--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -1,22 +1,43 @@
 import React from "react";
 import Image from "next/image";
 
-const Review = () => {
+const reviews = [
+  { imgSrc: "/pexels-artempodrez-8533095.png", name: "fs", title: "rw", description: "Acme Chemicals has been a reliable supplier for our\n laboratory. The quality of their products is top-notch\n and their customer service is exceptional." },
+  { imgSrc: "/pexels-artempodrez-8533095.png", name: "fs", title: "rew", description: "" },
+  { imgSrc: "/pexels-artempodrez-8533095.png", name: "fd", title: "rw", description: "" },
+  { imgSrc: "/pexels-artempodrez-8533095.png", name: "fs", title: "rewre", description: "" },
+];
+
+interface ReviewProps {
+  key: number;
+  imgSrc: string;
+  name: string;
+  title: string;
+  description: string;
+}
+
+const Review: React.FC<ReviewProps> = (props: ReviewProps) => {
+  const { key, imgSrc, name, title, description } = props;
   return (
-    <div className="bg-white p-6 mb-2 rounded-md border-stone-200 border-2">
+    <div
+      className="bg-white p-6 mb-2 rounded-md border-stone-200 border-2"
+      key={key}
+    >
       <div className=" flex flex-row">
         <Image
-          src="/pexels-artempodrez-8533095.png"
+          src={imgSrc}
           alt="avatar"
           width={56}
           height={56}
         />
         <div className=" ml-4">
-          <div className="font-semibold">Name</div>
-          <div className="text-sm">Title</div>
+          <div className="font-semibold">{name}</div>
+          <div className="text-sm">{title}</div>
         </div>
       </div>
-      <p className="mt-2">Acme Chemicals has been a reliable supplier for our laboratory. The quality of their products is top-notch and their customer service is exceptional.</p>
+      <p className="mt-2">
+       {description}
+      </p>
     </div>
   );
 };
@@ -31,9 +52,15 @@ export const Testimonials = () => {
         Hear from our satisfied customers about their experience with Acme
         Chemicals.
       </p>
-      <Review />
-      <Review />
-      <Review />
+      {reviews.map((rev, index: number) => (
+        <Review
+          key={index}
+          imgSrc={rev.imgSrc}
+          name={rev.name}
+          title={rev.title}
+          description={rev.description}
+        />
+      ))}
     </div>
   );
 };
