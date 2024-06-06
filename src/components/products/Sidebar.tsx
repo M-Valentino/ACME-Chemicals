d0031727eb296d6406c6e05e7b6a6e47f35608c9
Mark Valentino <mike9377mike9377@gmail.com>
2024-06-05 23:17:23 -0400

    added subcategories

--- src/components/products/Sidebar.tsx
+++ src/components/products/Sidebar.tsx
@@ -11,6 +11,17 @@ interface SidebarProps {
 export const Sidebar: React.FC<SidebarProps> = (props: SidebarProps) => {
   const { priceRange, setPriceRange } = props;
 
+  const categories = [
+    {
+      category: "Industrial Chemicals",
+      subCategories: ["Acids", "Bases", "Solvents"],
+    },
+    {
+      category: "Laboratory Chemicals",
+      subCategories: ["Reagents", "buffers", "analytical chemicals"],
+    },
+  ];
+
   const priceInputFormatter = (val: string) => {
     return Math.min(parseInt(val.substring(1)), 300) || 0;
   };
@@ -24,26 +35,39 @@ export const Sidebar: React.FC<SidebarProps> = (props: SidebarProps) => {
     <div className=" max-w-[256px] mr-16">
       <h1 className=" font-extrabold text-5xl text-primary">Products</h1>
       <div className=" bg-slate-100 p-4 mt-4 rounded-md border-stone-200 border-2">
-        <h2 className=" mb-2 font-semibold text-lg ">Categories</h2>
-        <div className="flex align-items-center ">
-          <Checkbox inputId="ingredient1" name="pizza" value="Cheese" checked />
-          <label htmlFor="ingredient1" className="ml-2 ">
-            Cheese
-          </label>
-        </div>
-        <div className="flex align-items-center ">
-          <Checkbox inputId="ingredient1" name="pizza" value="Cheese" checked />
-          <label htmlFor="ingredient1" className="ml-2 ">
-            Cheese
-          </label>
-        </div>
-        <div className="flex align-items-center ">
-          <Checkbox inputId="ingredient1" name="pizza" value="Cheese" checked />
-          <label htmlFor="ingredient1" className="ml-2 ">
-            Cheese
-          </label>
-        </div>
-        <h2 className="mt-4 mb-2 font-semibold text-lg ">Price Range</h2>
+        <h2 className="font-semibold text-lg ">Categories</h2>
+        {categories.map((cat, i, row) => (
+          <>
+            <div className="flex align-items-center mt-2" key={cat.category}>
+              <Checkbox
+                inputId="ingredient1"
+                name="pizza"
+                value="Cheese"
+                checked
+              />
+              <label htmlFor="ingredient1" className="ml-2 ">
+                {cat.category}
+              </label>
+            </div>
+            {cat.subCategories.map((subCat) => (
+              <div className="flex align-items-center ml-6 mt-1" key={subCat}>
+                <Checkbox
+                  inputId="ingredient1"
+                  name="pizza"
+                  value="Cheese"
+                  checked
+                />
+                <label htmlFor="ingredient1" className="ml-2 text-gray-700">
+                  {subCat}
+                </label>
+              </div>
+            ))}
+            {i !== row.length - 1 && (
+              <hr className="border-stone-200 border-1 mt-1" />
+            )}
+          </>
+        ))}
+        <h2 className="mt-5 mb-2 font-semibold text-lg ">Price Range</h2>
         <div className="flex flex-row">
           <InputText
             value={`$${priceRange[0].toString()}`}
