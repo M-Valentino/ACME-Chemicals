7505e31b93af0716378406f08d49aaa65885a025
Mark Valentino <mike9377mike9377@gmail.com>
2024-06-14 20:45:43 -0400

    added auth check

--- src/components/MainWrapper.tsx
+++ src/components/MainWrapper.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode, useEffect, useState } from "react";
 import Head from "next/head";
 import { Footer } from "./Footer";
 import { TopNav } from "./TopNav";
+import { API_MESSAGES } from "@/utils/consts";
 
 interface MainWrapperProps {
   children: ReactNode;
@@ -14,6 +15,22 @@ export const MainWrapper: React.FC<MainWrapperProps> = (props) => {
   const [sessionInfo, setSessionInfo] = useState<string>("");
 
   useEffect(() => {
+    fetch(`/api/auth`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.message !== API_MESSAGES.success) {
+          localStorage.removeItem("sessionInfo");
+        }
+      });
+  }, []);
+
+  useEffect(() => {
     if (typeof window !== "undefined") {
       const storedSessionInfo = localStorage.getItem("sessionInfo");
       if (storedSessionInfo) {
@@ -24,10 +41,10 @@ export const MainWrapper: React.FC<MainWrapperProps> = (props) => {
 
   return (
     <>
-    <Head>
-      <title>{title}</title>
-    </Head>
-      <TopNav title={title} sessionInfo={sessionInfo}/>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <TopNav title={title} sessionInfo={sessionInfo} />
       <main className="mt-12 min-h-[80vh]">{children}</main>
       <Footer />
     </>
