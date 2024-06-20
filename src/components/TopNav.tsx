345a236b24fa60f2c393c9422cb635181920f66c
Mark Valentino <mike9377mike9377@gmail.com>
2024-06-20 04:13:56 -0400

    added notification bell

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -90,15 +90,29 @@ const CurentUserUI = ({ sessionInfo }: CurentUserUIProps) => {
     { name: "Log Out", href: "/logout" },
   ];
   return (
-    <>
+    <div className="ml-1 w-[111.333px] flex flex-row justify-between">
+      <div className=" ml-8 flex flex-col justify-center  text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-7 cursor-pointer"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.25 9a6.75 6.75 0 0 1 13.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 0 1-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 1 1-7.48 0 24.585 24.585 0 0 1-4.831-1.244.75.75 0 0 1-.298-1.205A8.217 8.217 0 0 0 5.25 9.75V9Zm4.502 8.9a2.25 2.25 0 1 0 4.496 0 25.057 25.057 0 0 1-4.496 0Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
       <div
-        className="flex flex-col justify-center ml-1 w-[111.333px]"
+        className="flex flex-col justify-center"
         onClick={() => setUserMenuOpen(!userMenuOpen)}
       >
         <Avatar
           label={sessionInfo.substring(0, 1)}
           shape="circle"
-          className="cursor-pointer transition ease-in-out hover:scale-105 hover:shadow-lg ml-auto"
+          className="cursor-pointer transition ease-in-out hover:scale-105 hover:shadow-lg"
         />
       </div>
       {userMenuOpen && (
@@ -111,7 +125,7 @@ const CurentUserUI = ({ sessionInfo }: CurentUserUIProps) => {
           />
         </div>
       )}
-    </>
+    </div>
   );
 };
 
