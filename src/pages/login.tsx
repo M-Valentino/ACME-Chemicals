6d4db82bc5e61b68cdec40a4d8833b15dce0df12
Mark Valentino <mike9377mike9377@gmail.com>
2024-06-19 20:00:24 -0400

    made header more readable with white glow

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,7 +7,13 @@ export default function Login() {
   const [showLogInPanel, setShowLogInPanel] = useState<Boolean>(true);
   return (
     <MainWrapper title={showLogInPanel ? "Log In" : "Register"} showBG>
-      <h1 className="pt-12 text-6xl text-primary font-extrabold text-center">
+      <h1
+        className="pt-12 text-6xl text-primary font-extrabold text-center"
+        style={{
+          textShadow:
+            "0 0 2px #fff, 0 0 4px #fff, 0 0 6px #fff, 0 0 8px #fff, 0 0 10px #fff, 0 0 12px #fff",
+        }}
+      >
         Welcome back!
       </h1>
       <div className="border-secondary bg-white border-2 max-w-[480px] p-8 m-auto rounded-md mt-8">
