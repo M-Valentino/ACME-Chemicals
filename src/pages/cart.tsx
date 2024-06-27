b2441f8f1740331406101f55af7d4e63f8286931
Mark Valentino <mike9377mike9377@gmail.com>
2024-06-26 22:20:58 -0400

    cart items are displayed

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
 import { MainWrapper } from "@/components/MainWrapper";
 import { getCurrentUser } from "@/utils/authFunctions";
 import { API_MESSAGES } from "@/utils/consts";
+import { ProductCardType } from "@/components/ProductCard";
 
 export default function Cart() {
-  const [cartItems, setCartItems] = useState<string>("");
+  const [cartItems, setCartItems] = useState<ProductCardType[]>([]);
 
   useEffect(() => {
     const currentUser = getCurrentUser();
@@ -20,7 +22,7 @@ export default function Cart() {
         .then((data) => {
           console.log(data);
           if (data.message === API_MESSAGES.success) {
-            setCartItems(JSON.stringify(data.cart.rows));
+            setCartItems(data.cart.rows);
           }
         })
         .catch((error) => {
@@ -28,9 +30,35 @@ export default function Cart() {
         });
     }
   }, []);
+
+  const CartItem: React.FC<ProductCardType> = (props: ProductCardType) => {
+    const { key, imgsrc, name } = props;
+    return (
+      <div key={key} className="flex flex-row mt-8">
+        <Image src={imgsrc} alt={name} width={100} height={100} />
+        <h2 className="ml-8 font-semibold text-lg">{name}</h2>
+      </div>
+    );
+  };
+
   return (
     <MainWrapper title="Cart">
-      <div>{cartItems}</div>
+      <div className="md:ml-8 xs:ml-4 ">
+        <h1 className="text-6xl text-primary font-extrabold">Cart</h1>
+        {cartItems.length > 0 &&
+          cartItems.map((product: ProductCardType, index: number) => (
+            <CartItem
+              id={product.id}
+              key={index}
+              imgsrc={product.imgsrc}
+              name={product.name}
+              description={product.description}
+              price={product.price}
+              discount={product.discount}
+              size={product.size}
+            />
+          ))}
+      </div>
     </MainWrapper>
   );
 }
